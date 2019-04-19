98a1bd9e32fd49151ef1e329ecb31b48090314ed
RyanV <6015315+rvvergara@users.noreply.github.com>
2019-04-19 12:11:49 +0800

    Improve configuration and directory structure

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRoutes from './routers/AppRouter';
 import 'normalize.css/normalize.css';
-import '../css/style.scss';
+import '../scss/style.scss';
 
 ReactDOM.render(<AppRoutes />, document.getElementById('app'));
