4e057f314e7c63a8205f0ef7e7964768554e8012
RyanV <6015315+rvvergara@users.noreply.github.com>
2019-04-22 13:25:12 +0800

    Connect removeExpense to ExpenseListItem

--- src/js/components/ExpenseListItem.js
+++ src/js/components/ExpenseListItem.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
 
 const ExpenseListItem = ({
+  id,
   description,
   amount,
   createdAt,
+  dispatch,
 }) => (
   <div>
     <h3>
@@ -23,7 +27,15 @@ When:
       {' '}
       {new Date(createdAt).toDateString()}
     </p>
+    <button
+      type="button"
+      onClick={() => {
+        dispatch(removeExpense({ id }));
+      }}
+    >
+      Delete
+    </button>
   </div>
 );
 
-export default ExpenseListItem;
+export default connect()(ExpenseListItem);
