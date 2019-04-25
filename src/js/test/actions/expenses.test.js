fa820dc653d310b6cd713f7359427ea2771291f6
RyanV <6015315+rvvergara@users.noreply.github.com>
2019-04-25 14:18:39 +0800

    Write test for expenses actions

--- src/js/test/actions/expenses.test.js
+++ src/js/test/actions/expenses.test.js
@@ -1,7 +1,47 @@
-import { addExpense } from '../../actions/expenses';
+import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
+
+describe('removeExpense', () => {
+  test('should setup remove expense action object', () => {
+    expect(removeExpense({ id: '293' })).toEqual({
+      type: 'REMOVE_EXPENSE',
+      id: '293',
+    });
+  });
+});
+
+describe('editExpense', () => {
+  test('should setup edit expense action', () => {
+    const action = editExpense('023Arww', { amount: '35' });
+    expect(action.type).toBe('EDIT_EXPENSE');
+    expect(action.updates.amount).toBe('35');
+  });
+});
 
 describe('addExpense', () => {
-  test('1 + 1 is 2', () => {
-    expect(1 + 1).toBe(2);
+  test('it should set up addExpense action w/ given values', () => {
+    const expense = {
+      description: 'Cable bill',
+      amount: '100',
+      createdAt: 58877,
+      note: 'Coffee',
+    };
+    const action = addExpense(expense);
+    expect(action).toEqual({
+      type: 'ADD_EXPENSE',
+      expense: { ...expense, id: expect.any(String) },
+    });
+  });
+
+  test('it should return an action with default values for expenses if no argument was passed', () => {
+    expect(addExpense()).toEqual({
+      type: 'ADD_EXPENSE',
+      expense: {
+        id: expect.any(String),
+        description: '',
+        amount: 0,
+        createdAt: 0,
+        note: '',
+      },
+    });
   });
 });
