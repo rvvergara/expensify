603607b41c1b7caded6393ee3c9324759ab0ed62
RyanV <6015315+rvvergara@users.noreply.github.com>
2019-04-25 16:52:35 +0800

    Write tests for expenses reducer

--- src/js/test/selectors/expenses.test.js
+++ src/js/test/selectors/expenses.test.js
@@ -1,32 +1,11 @@
 import moment from 'moment';
 import getVisibleExpenses from '../../selectors/expenses';
+import expensesData from '../fixtures/expenses';
 
 describe('getVisibleExpenses', () => {
   let expenses;
   beforeEach(() => {
-    expenses = [
-      {
-        id: 1,
-        description: 'Gum',
-        amount: 195,
-        note: '',
-        createdAt: 0,
-      },
-      {
-        id: 2,
-        description: 'Rent',
-        amount: 109500,
-        note: '',
-        createdAt: moment(0).subtract(4, 'days').valueOf(),
-      },
-      {
-        id: 3,
-        description: 'Credit card',
-        amount: 4500,
-        note: '',
-        createdAt: moment(0).add(4, 'days').valueOf(),
-      },
-    ];
+    expenses = expensesData;
   });
 
   test('should filter by text value', () => {
