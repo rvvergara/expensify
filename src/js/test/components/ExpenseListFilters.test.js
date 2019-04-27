b245e08364c262dc23014df9606da0de345a2a8c
RyanV <6015315+rvvergara@users.noreply.github.com>
2019-04-27 14:42:33 +0800

    Add test for onDatesChange in ExpenseFilters

--- src/js/test/components/ExpenseListFilters.test.js
+++ src/js/test/components/ExpenseListFilters.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import moment from 'moment';
 import { ExpenseListFilters } from '../../components/ExpenseListFilters';
 import { filters, altFilters } from '../fixtures/filters';
 
@@ -55,6 +56,14 @@ describe('ExpenseListFilters', () => {
     const value = 'amount';
     wrapper.find('select').simulate('change', { target: { value } });
     expect(sortByAmount).toHaveBeenCalled();
+  });
+
+  test('should handle date change', () => {
+    const startDate = moment(0).add(3, 'days');
+    const endDate = moment(0).add(5, 'days');
+    wrapper.find('withStyles(DateRangePicker)').prop('onDatesChange')({ startDate, endDate });
+    expect(setStartDate).toHaveBeenLastCalledWith(startDate);
+    expect(setEndDate).toHaveBeenLastCalledWith(endDate);
   });
 
   test('should handle date focus change', () => {
