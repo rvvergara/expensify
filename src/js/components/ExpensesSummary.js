3310b1f44841c7ea2a0396f939d794c8f6f8446d
RyanV <6015315+rvvergara@users.noreply.github.com>
2019-04-29 19:29:39 +0800

    Show dollar not cents in ExpensesSummary

--- src/js/components/ExpensesSummary.js
+++ src/js/components/ExpensesSummary.js
@@ -5,23 +5,24 @@ import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import expensesTotal from '../selectors/expenses-total';
 
-export const ExpensesSummary = (props) => {
-  const { total, count } = props;
-  return (
-    <div>
-      Showing
+export const ExpensesSummary = ({ total, count }) => (
+  <div>
+    <h3>
+        Showing
       {' '}
       { count }
       {' '}
-items totalling
+      {count !== 1 ? 'items' : 'item'}
+      {' '}
+totalling
       {' '}
       { numeral(total).format('$0,0.00')}
-    </div>
-  );
-};
+    </h3>
+  </div>
+);
 
 const mapStateToProps = state => ({
-  total: expensesTotal(selectExpenses(state.expenses, state.filters)),
+  total: expensesTotal(selectExpenses(state.expenses, state.filters)) / 100,
   count: selectExpenses(state.expenses, state.filters).length,
 });
 
