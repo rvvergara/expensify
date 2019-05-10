f854e9b4d3aaa86b2bd2368c127c8e8b370fa64c
RyanV <6015315+rvvergara@users.noreply.github.com>
2019-05-10 16:23:41 +0800

    Add loader

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter, { history } from './routers/AppRouter';
+import LoadingPage from './components/LoadingPage';
 import configureStore from './store/configureStore';
 import {
   startSetExpenses,
@@ -29,7 +30,7 @@ const renderApp = () => {
   }
 };
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
 
 firebase.auth().onAuthStateChanged((user) => {
