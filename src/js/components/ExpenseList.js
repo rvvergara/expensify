de66a344a601aaf394e1a7c7eb36120988a2786e
RyanV <6015315+rvvergara@users.noreply.github.com>
2019-05-10 16:01:56 +0800

    Style list

--- src/js/components/ExpenseList.js
+++ src/js/components/ExpenseList.js
@@ -4,19 +4,28 @@ import selectExpenses from '../selectors/expenses';
 import ExpenseListItem from './ExpenseListItem';
 
 export const ExpenseList = ({ expenses }) => (
-  <div>
-    {
-      expenses.length === 0 ? (
-        <p>No expenses</p>
-      ) : (
-        expenses.map(expense => (
-          <ExpenseListItem
-            key={expense.id}
-            {...expense}
-          />
-        ))
-      )
-    }
+  <div className="content-container">
+    <div className="list-header">
+      <div className="show-for-mobile">Expenses</div>
+      <div className="show-for-desktop">Expense</div>
+      <div className="show-for-desktop">Amount</div>
+    </div>
+    <div className="list-body">
+      {
+        expenses.length === 0 ? (
+          <div className="list-item list-item--message">
+            <span>No expenses</span>
+          </div>
+        ) : (
+          expenses.map(expense => (
+            <ExpenseListItem
+              key={expense.id}
+              {...expense}
+            />
+          ))
+        )
+      }
+    </div>
 
   </div>
 );
--- src/js/components/ExpenseListItem.js
+++ src/js/components/ExpenseListItem.js
@@ -7,26 +7,20 @@ const ExpenseListItem = ({
   id,
   description,
   amount,
-  note,
   createdAt,
 }) => (
-  <div>
-    <h3>
-Item:
-      {' '}
-      <Link to={`/edit/${id}`}>{description}</Link>
-    </h3>
-    <p>
-      {note}
-    </p>
-    <p>
+  <Link
+    className="list-item"
+    to={`/edit/${id}`}
+  >
+    <div>
+      <h3 className="list-item__title">{description}</h3>
+      <span className="list-item__subtitle">{ moment(createdAt).format('MMMM Do, YYYY') }</span>
+    </div>
+    <h3 className="list-item__data">
       {numeral(amount / 100).format('$0,0.00')}
-    </p>
-    <p>
-      { moment(createdAt).format('MMMM Do, YYYY') }
-    </p>
-
-  </div>
+    </h3>
+  </Link>
 );
 
 export default ExpenseListItem;
